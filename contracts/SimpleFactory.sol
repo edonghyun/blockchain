5037079ecf5a3743aa29f6faa561e20a8ed01078
donghyun <edonghyun@naver.com>
2021-10-03 01:10:52 +0900

    initial commit

---
+++ contracts/SimpleFactory.sol
@@ -0,0 +1,29 @@
+// SPDX-License_Identifier: minutes
+
+pragma solidity >=0.6.0 <0.9.0;
+
+import "./SimpleStorage.sol";
+
+contract StorageFactory is SimpleStorage {
+    SimpleStorage[] public simpleStorageArray;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage simpleStorage = new SimpleStorage();
+        simpleStorageArray.push(simpleStorage);
+    }
+
+    function sfStore(uint32 _simpleStorageIndex, uint256 _simpleStorageNumber)
+        public
+    {
+        SimpleStorage simpleStorage = SimpleStorage(
+            address(simpleStorageArray[_simpleStorageIndex])
+        );
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
+        return
+            SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]))
+                .retrieve();
+    }
+}
