0235e9480237c8cd5a628b02e2e00d2964a21c7d
donghyun <edonghyun@naver.com>
2021-10-03 04:39:42 +0900

    add owner and withdraw function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,33 +4,35 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-
 contract FundMe {
     AggregatorV3Interface internal priceFeed;
-    
-    constructor() {
-        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
-    }
-
 
     mapping(address => uint256) public addressToAmountFunded;
-    
+    address public owner;
+
+    constructor() {
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
+        owner = msg.sender;
+    }
+
     function fund() public payable {
+        uint256 minimumUSD = 50 * 10 ** 18;
+        require(msg.value > 0, "You need to spend more USD");
         addressToAmountFunded[msg.sender] += msg.value;
     }
-    
+
     function getVersion() public view returns (uint256) {
         return priceFeed.version();
     }
         
     function getPrice() public view returns (uint256) {
         (,int256 answer,,,) = priceFeed.latestRoundData();
-        return uint256(3);
+        return uint256(answer * 10000000000);
     }
     
     function getConversionRate(uint256 ethAmount) public view returns (uint256) {
         uint256 ethPrice = getPrice();
-        uint256 ethAmountInUsd = (ethPrice * ethAmount);
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         return ethPrice;
     }
     
@@ -43,6 +45,11 @@ contract FundMe {
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
         return price;
+    }
+    
+    function withdraw() payable public {
+        require(msg.sender == owner);
+        payable(msg.sender).transfer(address(this).balance);
     }
     
 }
